a6dca01fa9f6f0d2bfe6f8d593eb23ee6223fe0e
Federico <me@fregante.com>
2024-07-14 15:37:28 +0700

    Fix "getFullData" types

--- index.d.ts
+++ index.d.ts
@@ -11,19 +11,38 @@ export type TreeResult<T> = {
 	truncated: boolean;
 } & T[];
 
-// Feel free to contribute fields to this type
-export type GitObject = {
-	[other: string]: unknown;
+export type TreeResponseObject = {
 	path: string;
+	mode: string;
+	type: string;
+	size: number;
+	sha: string;
 	url: string;
+};
+
+export type ContentsReponseObject = {
+	name: string;
+	path: string;
+	sha: string;
+	size: number;
+	url: string;
+	html_url: string;
+	git_url: string;
+	download_url: string;
+	type: string;
+	_links: {
+		self: string;
+		git: string;
+		html: string;
+	};
 };
 
 export function getDirectoryContentViaContentsApi<T extends ListGithubDirectoryOptions>(options: T):
 T['getFullData'] extends true ?
-	Promise<GitObject[]> :
+	Promise<ContentsReponseObject[]> :
 	Promise<string[]>;
 
 export function getDirectoryContentViaTreesApi<T extends ListGithubDirectoryOptions>(options: T):
 T['getFullData'] extends true ?
-	Promise<TreeResult<GitObject>> :
+	Promise<TreeResult<TreeResponseObject>> :
 	Promise<TreeResult<string>>;
